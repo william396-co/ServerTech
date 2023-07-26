3d24af4982f4a43492721290e17e8b7f318d7917
huiping.liu <huiping.liu@silverkeyz.com>
2023-07-26 19:25:23 +0800

    epoll one shot

--- io/epoll_oneshot.c
+++ io/epoll_oneshot.c
@@ -13,7 +13,13 @@
 #include <pthread.h>
 
 #define MAX_EVENT_NUMBER 1024
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 1024
+
+struct fds
+{
+    int epollfd;
+    int sockfd;
+};
 
 // set fd nonblock mode
 int setnonblocking( int fd )
@@ -47,71 +53,35 @@ void reset_oneshot( int epollfd, int fd )
 /* 工作线程 */
 void * worker( void * arg )
 {
-    int sockfd = ( (fds *)arg )->sockfd;
-}
+    int sockfd = ( (struct fds *)arg )->sockfd;
+    int epollfd = ( (struct fds *)arg )->epollfd;
+    printf( "start new thread to receive data on fd:%d\n", sockfd );
 
-// LT 模式的工作流程
-void lt( struct epoll_event * events, int number, int epollfd, int listenfd )
-{
     char buf[BUFFER_SIZE];
-    for ( int i = 0; i < number; ++i ) {
-        int sockfd = events[i].data.fd;
-        if ( sockfd == listenfd ) {
-            struct sockaddr_in faddr;
-            socklen_t faddrlen = sizeof( faddr );
-            int connfd = accept( listenfd, (struct sockaddr *)&faddr, &faddrlen );
-            addfd( epollfd, connfd, 0 );
-        } else if ( events[i].events & EPOLLIN ) {
-            // 只要socket读缓存中还有未读出的数据，这段代码就被触发
-            printf( "event trigger once\n" );
-            memset( buf, 0, sizeof( buf ) );
-            int ret = recv( sockfd, buf, sizeof( buf ) - 1, 0 );
-            if ( ret <= 0 ) {
-                close( sockfd );
-                continue;
-            }
-            printf( "get %d bytes of contents:[ %s]\n", ret, buf );
-        } else {
-            printf( "something else happened\n" );
-        }
-    }
-}
+    memset( buf, 0, BUFFER_SIZE );
 
-// ET模式的工作流程
-void et( struct epoll_event * events, int number, int epollfd, int listenfd )
-{
-    char buf[BUFFER_SIZE];
-    for ( int i = 0; i < number; ++i ) {
-        int sockfd = events[i].data.fd;
-        if ( sockfd == listenfd ) {
-            struct sockaddr_in faddr;
-            socklen_t faddrlen = sizeof( faddr );
-            int connfd = accept( listenfd, (struct sockaddr *)&faddr, &faddrlen );
-            addfd( epollfd, connfd, 1 ); /*  对connfd开启ET模式 */
-        } else if ( events[i].events & EPOLLIN ) {
-            /* 这段代码不会被重复触发，所以我们循环读取数据，以确保把socket读缓存中的所有数据读出 */
-            printf( "event trigger once\n" );
-            while ( 1 ) {
-                memset( buf, 0, sizeof( buf ) );
-                int ret = recv( sockfd, buf, sizeof( buf ) - 1, 0 );
-                if ( ret < 0 ) {
-                    /*  对于非阻塞IO，下面的条件成立表示数据已经全部读取完毕，此后，epoll就能再次触发sockfd上的EPOLLIN事件，以驱动下一次读操作*/
-                    if ( ( errno == EAGAIN ) || ( errno == EWOULDBLOCK ) ) {
-                        printf( "read later\n" );
-                        break;
-                    }
-                    close( sockfd );
-                    break;
-                } else if ( ret == 0 ) {
-                    close( sockfd );
-                } else {
-                    printf( "get %d bytes of contents: [%s]\n", ret, buf );
-                }
+    // 循环读取sockfd上的数据,直到遇到EAGAIN错误
+    while ( 1 ) {
+
+        int ret = recv( sockfd, buf, BUFFER_SIZE - 1, 0 );
+        if ( 0 == ret ) {
+            close( sockfd );
+            printf( "foreiner closed the connection\n" );
+            break;
+        } else if ( ret < 0 ) {
+            if ( errno == EAGAIN ) {
+                reset_oneshot( epollfd, sockfd );
+                printf( "read later\n" );
+                break;
             }
         } else {
-            printf( "something else happened\n" );
+            printf( "get content: %s\n", buf );
+            // 休眠5s 模拟数据处理过程
+            sleep( 5 );
         }
     }
+    printf( "end thread receiving data on fd: %d\n", sockfd );
+    return NULL;
 }
 
 int main( int argc, char ** argv )
@@ -137,10 +107,13 @@ int main( int argc, char ** argv )
     ret = bind( listenfd, (struct sockaddr *)&addr, sizeof( addr ) );
     assert( ret != -1 && "bind failed" );
 
+    ret = listen( listenfd, 5 );
+    assert( ret != -2 && "listen failed" );
+
     struct epoll_event events[MAX_EVENT_NUMBER];
     int epollfd = epoll_create( 5 );
     assert( epollfd != -1 );
-    addfd( epollfd, listenfd, 1 );
+    addfd( epollfd, listenfd, 0 ); /* 注意，监听socket上是不能注册EPOLLONESHOT事件的，否则应用程序只能处理一个客户端连接，因为后续的客户连接请求将不再触发listenfd上的EPOLLIN事件 */
 
     while ( 1 ) {
         int ret = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
@@ -148,8 +121,28 @@ int main( int argc, char ** argv )
             printf( "epoll failure\n" );
             break;
         }
-        lt( events, ret, epollfd, listenfd ); /* LT 模式*/
-                                              // et(events,ret,epollfd,listenfd);/* ET 模式*/
+
+        for ( int i = 0; i < ret; ++i ) {
+            int sockfd = events[i].data.fd;
+            if ( sockfd == listenfd ) {
+                struct sockaddr_in laddr;
+                socklen_t addr_len = sizeof( laddr );
+                int connfd = accept( listenfd, (struct sockaddr *)&laddr, &addr_len );
+
+                /* 对每个非监听文件描述符都注册EPOLLONESHOT事件  */
+                addfd( epollfd, connfd, 1 );
+
+            } else if ( events[i].events & EPOLLIN ) {
+                pthread_t thread;
+                struct fds fds_new;
+                fds_new.epollfd = epollfd;
+                fds_new.sockfd = sockfd;
+                /* 新启动一个工作线程为sockfd服务 */
+                pthread_create( &thread, NULL, worker, (void *)&fds_new );
+            } else {
+                printf( "something else happened\n" );
+            }
+        }
     }
     close( listenfd );
     return 0;
