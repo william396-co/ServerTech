3bdae869c7d968cbcf2bdc890592779087aabdd3
huiping.liu <huiping.liu@silverkeyz.com>
2023-09-04 18:17:29 +0800

    day 0

---
+++ io/linux/day0/server.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string>
+
+int main()
+{
+    std::cout << " server\n";
+
+    int sockfd = socket( AF_INET, SOCK_STREAM, 0 );
+
+    struct sockaddr_in s_addr;
+    // bzero( &s_addr, sizeof( s_addr ) );
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+    s_addr.sin_port = htons( 9527 );
+
+    bind( sockfd, (sockaddr *)&s_addr, sizeof( s_addr ) );
+
+    listen( sockfd, SOMAXCONN );
+
+    struct sockaddr_in c_addr;
+    socklen_t c_addr_len = sizeof( c_addr );
+    //    bzero( &c_addr, c_addr_len );
+
+    int client_fd = accept( sockfd, (sockaddr *)&c_addr, &c_addr_len );
+
+    std::cout << "new client fd: " << client_fd << " Ip :" << inet_ntoa( c_addr.sin_addr ) << " Port:" << ntohs( c_addr.sin_port ) << std::endl;
+
+    return 0;
+}
