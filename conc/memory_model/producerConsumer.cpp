68d1757b65dec632c25585c1447fca627356c60d
jack <hpl.jack@gmail.com>
2023-10-04 13:47:04 +0800

    producer consumer(seq-cst)

---
+++ conc/memory_model/producerConsumer.cpp
@@ -0,0 +1,32 @@
+#include <atomic>
+#include <iostream>
+#include <string>
+#include <thread>
+
+std::string work;
+std::atomic<bool> ready { false };
+
+void consumer()
+{
+    while ( !ready.load() )
+        ;
+    std::cout << work << "\n";
+}
+
+void producer()
+{
+    work = "done";
+    ready = true;
+}
+
+int main()
+{
+
+    std::thread prod( producer );
+    std::thread con( consumer );
+
+    prod.join();
+    con.join();
+
+    return 0;
+}
