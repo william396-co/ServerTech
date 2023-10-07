6c374dbd9a6b3cbb9d3d90a1f7ec4bdd9f26f38e
huiping.liu <huiping.liu@silverkeyz.com>
2023-10-07 13:41:07 +0800

    dead lock/dead lock resolved

---
+++ conc/multithreading/deadlockResolved.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <chrono>
+#include <mutex>
+#include <thread>
+
+struct CriticalData
+{
+    std::mutex mut;
+};
+
+void deadLock( CriticalData & a, CriticalData & b )
+{
+
+    std::unique_lock lock1( a.mut, std::defer_lock );
+    std::cout << "thread: " << std::this_thread::get_id() << " first mutex\n";
+    std::this_thread::sleep_for( std::chrono::milliseconds { 1 } );
+
+    std::unique_lock lock2( b.mut, std::defer_lock );
+    std::cout << "thread: " << std::this_thread::get_id() << " second mutex\n";
+
+    std::cout << "   Thread: " << std::this_thread::get_id() << " get both mutex\n";
+
+    std::lock( lock1, lock2 );
+}
+
+int main()
+{
+
+    CriticalData c1;
+    CriticalData c2;
+    std::thread t1( [&] { deadLock( c1, c2 ); } );
+    std::thread t2( [&] { deadLock( c2, c1 ); } );
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
