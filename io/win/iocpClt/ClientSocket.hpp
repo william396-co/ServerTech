58d61391ee0df42959073bf15beb6b1d97924264
huiping.liu <huiping.liu@silverkeyz.com>
2023-11-23 19:37:18 +0800

    add some test code for iocp

---
+++ io/win/iocpClt/ClientSocket.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Socket.hpp"
+
+
+class ClientSocket : public Socket {
+
+};
--- io/win/iocpSrv/main.cpp
+++ io/win/iocpSrv/main.cpp
@@ -1,5 +1,11 @@
 #include "Master.hpp"
 
+/*
+* https://zhuanlan.zhihu.com/p/40096515
+* 
+* https://developer.aliyun.com/article/708589
+*/
+
 int main(int argc,char**argv)
 {
 	Master master;
