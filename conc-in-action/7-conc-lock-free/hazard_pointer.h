581caf60efe0c6406b87b5a359f461cd4142c4c3
huiping.liu <huiping.liu@silverkeyz.com>
2024-01-08 19:27:13 +0800

    hazard pointer

---
+++ conc-in-action/7-conc-lock-free/hazard_pointer.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <thread>
+#include <atomic>
+#include <stdexcept>
+
+constexpr auto max_hazard_pointers = 100;
+struct hazard_pointer
+{
+    std::atomic<std::thread::id> id;
+    std::atomic<void *> pointer;
+};
+hazard_pointer hazard_pointers[max_hazard_pointers];
+
+class hp_owner
+{
+public:
+    hp_owner( hp_owner const & ) = delete;
+    hp_owner & operator=( hp_owner const & ) = delete;
+
+    hp_owner()
+        : hp { nullptr }
+    {
+        for ( auto i = 0; i != max_hazard_pointers; ++i ) {
+            std::thread::id old_id;
+            if ( hazard_pointers[i].id.compare_exchange_strong( old_id, std::this_thread::get_id() ) ) {
+                hp = &hazard_pointers[i];
+                break;
+            }
+        }
+        if ( !hp ) {
+            throw std::runtime_error( "No Hazard pointers available" );
+        }
+    }
+    ~hp_owner()
+    {
+        hp->pointer.store( nullptr );
+        hp->id.store( std::thread::id() );
+    }
+    std::atomic<void *> & get_pointer()
+    {
+        return hp->pointer;
+    }
+
+private:
+    hazard_pointer * hp;
+};
+
+std::atomic<void *> & get_hazard_pointer_for_current_thread()
+{
+    thread_local static hp_owner hazard;
+    return hazard.get_pointer();
+}
+
+bool outstanding_hazard_pointers_for( void * p )
+{
+    for ( auto i = 0; i != max_hazard_pointers; ++i ) {
+        if ( hazard_pointers[i].pointer.load() == p ) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+void do_delete( void * p )
+{
+    delete static_cast<T *>( p );
+}
+
+struct data_to_reclaim
+{
+    void * data;
+    std::function<void( void * )> deleter;
+    data_to_reclaim * next;
+    template<typename T>
+    data_to_reclaim( T * p )
+        : data { p }, deleter { &do_deleter<T> }, next { nullptr }
+    {
+    }
+    ~data_to_reclaim()
+    {
+        deleter( data );
+    }
+};
+
+std::atomic<data_to_reclaim *> nodes_to_reclaim;
+void add_to_reclaim_list( data_to_reclaim * node )
+{
+}
+template<typename T>
+void reclaim_later( T * data )
+{
+}
+
+void delete_nodes_with_no_hazards()
+{
+}
--- conc-in-action/7-conc-lock-free/lock_free_stack.h
+++ conc-in-action/7-conc-lock-free/lock_free_stack.h
@@ -3,6 +3,9 @@
 #include <atomic>
 #include <memory>
 
+#define USE_OPT
+
+#ifndef USE_OPT
 template<typename T>
 class lock_free_stack
 {
@@ -33,5 +36,150 @@ private:
     };
     std::atomic<node *> head;
 };
+
+#else
+#define USE_HAZARD_POINTER
+#ifndef USE_HAZARD_POINTER
+
+template<typename T>
+class lock_free_stack
+{
+public:
+    void push( T const & data )
+    {
+        node * const new_node = new node( data );
+        new_node->next = head.load();
+        while ( !head.compare_exchange_weak( new_node->next, new_node ) )
+            ;
+    }
+
+    std::shared_ptr<T> pop()
+    {
+        ++threads_in_pop;
+        node * old_head = head.load();
+        while ( old_head && !head.compare_exchange_weak( old_head, old_head->next ) )
+            ;
+        std::shared_ptr<T> res;
+        if ( olde_head ) {
+            res.swap( old_head->data );
+        }
+        try_reclaim( old_head );
+        return res;
+    }
+
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        node * next;
+        explicit node( T const & data_ )
+            : data { std::make_shared<T>( data_ ) } {}
+    };
+    std::atomic<node *> head;
+
+private:
+    std::atomic<unsigned> threads_in_pop;
+    std::atomic<node *> to_be_deleted;
+    void try_reclaim( node * old_head )
+    {
+        if ( threads_in_pop == 1 ) {
+            node * nodes_to_delete = to_be_deleted.exchange( nullptr );
+            if ( !--threads_in_pop ) {
+                delete_nodes( nodes_to_delete );
+            } else if ( nodes_to_delete ) {
+                chain_pending_nodes( nodes_to_delete );
+            }
+            delete old_head;
+        } else {
+            chain_pending_node( olde_head );
+            --threads_in_pop;
+        }
+    }
+
+    void chain_pending_nodes( node * nodes )
+    {
+        node * last = nodes;
+        while ( node * const next = last->next ) {
+            last = next;
+        }
+        chain_pending_nodes( nodes, last );
+    }
+
+    void chain_pending_nodes( node * first, node * last )
+    {
+        last->next = to_be_deleted;
+        while ( !to_be_deleted.compare_exchange_weak( last->next, first ) )
+            ;
+    }
+    void chain_pending_node( node * n )
+    {
+        chain_pending_nodes( n, n );
+    }
+    static void delete_nodes( node * nodes )
+    {
+        while ( nodes ) {
+            node * next = nodes->next;
+            delete nodes;
+            nodes = next;
+        }
+    }
+};
+
+#else
+#include "hazard_pointer.h"
+
+template<typename T>
+class lock_free_stack
+{
+public:
+    void push( T const & data )
+    {
+        node * const new_node = new node( data );
+        new_node->next = head.load();
+        while ( !head.compare_exchange_weak( new_node->next, new_node ) )
+            ;
+    }
+
+    std::shared_ptr<T> pop()
+    {
+        std::atomic<void *> & hp = get_hazard_pointer_for_current_thread();
+        node * old_head = head.load();
+        do {
+            node * temp;
+            do {
+                temp = old_head;
+                hp.store( old_head );
+
+                old_head = head.load();
+            } while ( old_head != temp );
+
+        } while ( old_head && !head.compare_exchange_weak( old_head, old_head->next ) );
+        hp.store( nullptr );
+        std::shared_ptr<T> res;
+        if ( old_head ) {
+            res.swap( old_head->data );
+            if ( outstanding_hazard_pointers_for( old_head ) ) {
+                reclaim_later( old_head );
+            } else {
+                delete old_head;
+            }
+            delete_nodes_with_no_hazards();
+        }
+        return res;
+    }
+
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        node * next;
+        explicit node( T const & data_ )
+            : data { std::make_shared<T>( data_ ) } {}
+    };
+    std::atomic<node *> head;
+};
+
+#endif
+#endif
 
 void lock_free_stack_example();
