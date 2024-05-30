73510692fd2e172b45bcc4b76a0d9fd7db5ee675
huiping.liu <huiping.liu@silverkeyz.com>
2024-05-30 14:51:39 +0800

    modified sample

--- cpp-tmp/c01-introduction/hello_bin/hello_bin.cpp
+++ cpp-tmp/c01-introduction/hello_bin/hello_bin.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+#define USE_META
 #include "binary.hpp"
 
 int main()
 {
 
+#ifdef USE_META
     std::cout << binary<101010>::value << "\n";
+
+#else
+    std::cout << binary( 101010 ) << "\n";
+#endif
 
     return 0;
 }
