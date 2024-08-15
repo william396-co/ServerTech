78e43abb846f218756ba52f0506b01b740563c60
jack <hpl.jack@gmail.com>
2024-08-16 01:14:58 +0800

    more code

--- Effective-Modern-Cpp/C02-auto/Item5/Item5.cpp
+++ Effective-Modern-Cpp/C02-auto/Item5/Item5.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <vector>
 
 /*
  *  Item5: Prefer auto to explicit type declarations
  */
+
+class Widget
+{};
 
 int main()
 {
 
     // auto variable have their type deduced from their initializer, so they must be initialized.
     int x1;      // potentially uninitizlied
-   // auto x2;     // error! initializer required
+                 // auto x2;     // error! initializer required
     auto x3 = 0; // fine, x's value is well-defined
+
+    // use auto to replace std::function<> 
+    // std::function<> uses more memory than auto-declared object
+    // std::function<> approach
+    auto derefUpLess = []( std::unique_ptr<Widget> const & p1, std::unique_ptr<Widget> const & p2 ) { return *p1 < *p2; };
+    // std::function<bool(const std::unique_ptr<Widget>&,const std::unique_ptr<Widget>&)> = []( std::unique_ptr<Widget> const & p1, std::unique_ptr<Widget> const & p2 ) { return *p1 < *p2; };
 
     return 0;
 }
