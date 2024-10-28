c675dda38449e06d49ea58ab81d7936f0a141ff8
huiping.liu <huiping.liu@silverkeyz.com>
2024-10-28 17:20:39 +0800

    fixed

--- util/xtime.h
+++ util/xtime.h
@@ -79,7 +79,7 @@ inline bool isOverDay( time_t timestamp, int offset )
     return false;
 }
 
-inline isLeapYear( int year )
+inline bool isLeapYear( int year )
 {
     return ( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0;
 }
