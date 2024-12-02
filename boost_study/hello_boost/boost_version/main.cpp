0173d37b6fa2f7c1eb8f7f14c891cfab3b4062f6
Will369 <will_369@outlook.com>
2024-12-02 12:36:24 +0800

    test boost version example

---
+++ boost_study/hello_boost/boost_version/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/version.hpp>
+
+
+using namespace std;
+
+int main(){
+
+	cout<<"Hello World!\n";
+	cout<<"Boost version:"<< BOOST_VERSION<<"\n";
+
+	return 0;
+}
